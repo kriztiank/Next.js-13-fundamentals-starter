a491eb722e0303d278964e23f3a971d2356588ef
kriztiank <kriztiank@gmail.com>
2023-01-21 09:23:48 +0100

    Adding fonts, images and styling

--- app/conference/layout.js
+++ app/conference/layout.js
@@ -1,12 +1,9 @@
-import style from "./styles.css";
+import style from './styles.css'
 
 export default function ConferenceLayout({ children }) {
   return (
     <>
-      <header style={style.header}>
-        <h1>GLOBOMANTICS MANIACALLY TAKING TECH TO THE GLOBE</h1>
-      </header>
       <section>{children}</section>
     </>
-  );
+  )
 }

18025f94dd8d00c2d60098cf9649f72d6fd34db6
kriztiank <kriztiank@gmail.com>
2023-01-21 11:06:17 +0100

    Data fetching with revalidation

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -5,7 +5,7 @@ import styles from '../conference.module.css'
 async function fetchSpeakers() {
   const response = await fetch(
     'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json',
-    // { next: { revalidate: 20 } }
+    { next: { revalidate: 20 } }
   )
 
   const data = await response.json()
